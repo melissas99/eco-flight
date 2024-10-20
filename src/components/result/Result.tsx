c498e6cc57a6dc342e1e951dbcc0a54ff4029b2c
melissas99 <melissaspagnolo999@gmail.com>
2024-10-20 21:41:29 +0200

    feat: improve results style

--- src/components/result/Result.tsx
+++ src/components/result/Result.tsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import './Result.css';
 
 interface ResultProps {
@@ -6,17 +5,20 @@ interface ResultProps {
     co2Total: number;
 }
 
-const Result: React.FC<ResultProps> = ({ co2PerPassenger, co2Total }) => {
+const Result = ({ co2PerPassenger, co2Total }: ResultProps) => {
+    if (co2PerPassenger === 0 && co2Total === 0) {
+        return <div className="info">Select the airports and passengers to see the results</div>;
+    }
     return (
         <div className="result">
-            <p>
-                <span className="label">CO2 per passenger:</span>
-                <span>{co2PerPassenger.toFixed(2)} kg</span>
-            </p>
-            <p className="co2-total">
-                <span className="label">Total CO2:</span>
-                <span>{co2Total.toFixed(2)} kg</span>
-            </p>
+            <div className="result-item co2-per-passenger">
+                <p className="label">CO2 per passenger</p>
+                <p className="value">{co2PerPassenger.toFixed(2)} kg</p>
+            </div>
+            <div className="result-item co2-total">
+                <p className="label">Total CO2</p>
+                <p className="value">{co2Total.toFixed(2)} kg</p>
+            </div>
         </div>
     );
 };
